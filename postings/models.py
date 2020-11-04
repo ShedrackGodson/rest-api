356b6c368e1b99bbbeccfa06734f21fe67383a80
ShedrackGodson <hedrickgodson23@gmail.com>
2020-11-05 00:16:02 +0300

    postings app and BlogPost model

---
+++ postings/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+
+
+class BlogPost(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    content = models.TextField(max_length=255, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user
+
+    class Meta:
+        ordering = [
+            "-id"
+        ]
+    
--- rest/settings.py
+++ rest/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'postings.apps.PostingsConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,5 +127,10 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framewotk.authentication.BasicAuthentication',
     ]
 }
