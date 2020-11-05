64931a2b43d6870201711be62fb923dcc1448eab
ShedrackGodson <hedrickgodson23@gmail.com>
2020-11-05 23:42:23 +0300

    Search Queryset

--- postings/api/views.py
+++ postings/api/views.py
@@ -1,22 +1,35 @@
 from .serializer import BlogPostSerializer
 from rest_framework import generics, mixins
 from postings.models import BlogPost
+from django.db.models import Q
 
 
 class BlogPostListView(mixins.CreateModelMixin, generics.ListAPIView):
     serializer_class = BlogPostSerializer
 
     def get_queryset(self):
-        return BlogPost.objects.all()
+        qs = BlogPost.objects.all()
+        query = self.request.GET.get("q")
+        if query is not None:
+            qs = qs.filter(Q(title__icontains=query) | Q(content__icontains=query)).distinct()
+        return qs
     
     def post(self, request, *args, **kwargs): # Giving the ListAPIView ability to Create
         return self.create(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        print("Delete Fired!")
-    
+
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
+        
+    # def put(self, request, *args, **kwargs): # Giving the ListAPIView ability to Create
+    #     return self.update(request, *args, **kwargs)
+    
+    # def patch(self, request, *args, **kwargs): # Giving the ListAPIView ability to Create
+    #     return self.update(request, *args, **kwargs)
+    
+    # def delete(self, request, *args, **kwargs):
+    #     print("Delete Fired!")
+    
+    
 
 class BlogPostCreateView(generics.CreateAPIView):
     lookup_field = "pk"
